2926885f250059df2167c7c747fc16f020c7bad5
Revina Nataliia <natalyrevina@gmail.com>
2018-03-10 18:32:04 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  'use strict';
+  var p = base; 
+
+  for(var i = 2; i < base; i++){
+    if(base % i == 0){
+      var isSimple = true;
+      for (var j = 2; j < i; j++){
+        if (i % j == 0){
+          isSimple = false;
+          break;
+        }
+      }
+      if(isSimple){
+        p=i;
+      }
+    }
+  }
+  var counter = 0;
+  for (var i = p; number/i >= 1; i = i * p){
+      counter = counter + Math.floor(number/i);
+  }  
+  return counter;
 }
